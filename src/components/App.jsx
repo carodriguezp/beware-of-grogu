ed0e6b7dcd59532e9e65cfd9f1225fddef282d56
Mbueno992 <mbueno_92@hotmail.com>
2024-01-29 11:22:17 +0100

    create hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,37 @@
-import '../style/App.scss'
+import '../style/App.scss';
+import { useState } from 'react';
 
 function App() {
-  const user = "Carolina";
+  const arrayEgg = ['🥚', '🥚', '🥚'];
+  const arrayCockies = ['🍪', '🍪', '🍪'];
+  const arrayFrog = ['🐸', '🐸', '🐸'];
+  const [groguMove, setGroguMove] = useState(0);
+  const [goods, setGoods] = useState([arrayEgg, arrayCockies, arrayFrog]);
+  const [diceResult, setDiceResult] = useState(0);
+  const [gameResult, setGameResult] = useState('');
+  console.log(goods);
+
+  const rollDice = () => {
+    const diceNumber = Math.ceil(Math.random() * 4);
+
+    if (diceNumber === 1) {
+      setGoods(arrayCockies.slice(1), arrayEgg, arrayCockies);
+      setGameResult('Se ha descargado una galleta');
+    }
+    if (diceNumber === 2) {
+      setGoods(arrayCockies, goods[0].slice(1), arrayFrog);
+      setGameResult('Se ha descargado una huevo');
+      if (diceNumber === 3) {
+        setGoods(arrayCockies, arrayFrog.slice(1));
+        setGameResult('Se ha descargado una rana');
+      }
+      if (diceNumber === 4) {
+        setGameResult('Grogu avanza una casilla');
+      }
+    }
+  };
 
   return (
-
     <div className="page">
       <header>
         <h1>¡Cuidado con Grogu!</h1>
@@ -12,7 +39,9 @@ function App() {
 
       <main className="page">
         <section className="board">
-          <div className="cell"><div className="grogu">👣</div></div>
+          <div className="cell">
+            <div className="grogu">👣</div>
+          </div>
           <div className="cell"></div>
           <div className="cell"></div>
           <div className="cell"></div>
@@ -22,32 +51,33 @@ function App() {
         </section>
 
         <section>
-          <button className="dice">Lanzar Dado</button>
-          <div className="game-status">En curso</div>
+          <button className="dice" onClick={rollDice}>
+            Lanzar Dado
+          </button>
+          <div className="game-status">{gameResult}</div>
         </section>
 
         <section className="goods-container">
-          <div className="goods-item">🍪</div>
-          <div className="goods-item">🍪</div>
-          <div className="goods-item">🍪</div>
+          <div className="goods-item">{goods[1]}</div>
+          {/* <div className="goods-item">{goods[1]}</div>
+          <div className="goods-item">{goods[1]}</div> */}
         </section>
         <section className="goods-container">
-          <div className="goods-item">🥚</div>
-          <div className="goods-item">🥚</div>
-          <div className="goods-item">🥚</div>
+          <div className="goods-item">{goods[0]}</div>
+          {/* <div className="goods-item">{goods[0]}</div>
+          <div className="goods-item">{goods[0]}</div> */}
         </section>
         <section className="goods-container">
-          <div className="goods-item">🐸</div>
-          <div className="goods-item">🐸</div>
-          <div className="goods-item">🐸</div>
+          <div className="goods-item">{goods[2]}</div>
+          {/* <div className="goods-item">{goods[2]}</div>
+          <div className="goods-item">{goods[2]}</div> */}
         </section>
         <section>
           <button className="restart-button">Reiniciar Juego</button>
         </section>
       </main>
     </div>
-
   );
 }
 
-export default App
+export default App;
